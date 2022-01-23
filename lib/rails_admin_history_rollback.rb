91b8ce251ca07388173cb5e99e6fd3420754253f
Theeraphat Jantakat <pppangpondpp7@gmail.com>
2022-01-23 13:00:05 +0700

    add haml to dependency

--- lib/rails_admin_history_rollback.rb
+++ lib/rails_admin_history_rollback.rb
@@ -7,5 +7,7 @@ require 'rails_admin/config/actions'
 require 'rails_admin/extension'
 require 'rails_admin/extensions/paper_trail'
 
+require 'haml'
+
 require "rails_admin_history_rollback/extensions/paper_trail/auditing_adapter"
 require "rails_admin_history_rollback/config/actions/history_index"
